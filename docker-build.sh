b2caa350cc985cacbf801cbf89c516366d4941ef
rizkimufrizal <mufrizalrizki@gmail.com>
2018-01-06 19:28:15 +0700

    add command docker

---
+++ docker-build.sh
@@ -0,0 +1,5 @@
+gradle clean build docker -p Configuration-Server
+gradle clean build docker -p Discovery-Service
+gradle clean build docker -p Transaction-Service
+gradle clean build docker -p Catalog-Service
+gradle clean build docker -p OAuth2-Service
