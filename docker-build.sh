ed5cdac3724be7235722501382f8ff36e5dd2b19
rizkimufrizal <mufrizalrizki@gmail.com>
2018-01-06 22:11:36 +0700

    add API Gateway in docker build

--- docker-build.sh
+++ docker-build.sh
@@ -3,3 +3,4 @@ gradle clean build docker -p Discovery-Service
 gradle clean build docker -p Transaction-Service
 gradle clean build docker -p Catalog-Service
 gradle clean build docker -p OAuth2-Service
+gradle clean build docker -p API-Gateway
