2bc4df2eb3955d035423dd7709991cfeb41321c6
rizkimufrizal <mufrizalrizki@gmail.com>
2018-01-12 22:06:37 +0700

    delete discovery service eureka

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,4 @@
 gradle clean build docker -p Configuration-Server
-gradle clean build docker -p Discovery-Service
 gradle clean build docker -p Transaction-Service
 gradle clean build docker -p Catalog-Service
 gradle clean build docker -p OAuth2-Service
