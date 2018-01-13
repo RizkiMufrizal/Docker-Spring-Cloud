31581761a354180a48adb2370353c051cc8ee110
rizkimufrizal <mufrizalrizki@gmail.com>
2018-01-13 10:03:45 +0700

    change configuration server to consul

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,3 @@
-gradle clean build docker -p Configuration-Server
 gradle clean build docker -p Transaction-Service
 gradle clean build docker -p Catalog-Service
 gradle clean build docker -p OAuth2-Service
